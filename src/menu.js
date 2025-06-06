7eb73d6227c2797cd98bf22b9251935bbd99e641
diawara09 <diawaraholding223@gmail.com>
2025-06-06 09:17:00 +0300

    Finish restaurant page

--- src/menu.js
+++ src/menu.js
@@ -30,3 +30,45 @@ const menuArray = [
         price: 175
     },
 ]
+
+function menuPage(){
+    const menuEl = document.createElement("div")
+    const contentEl =  document.querySelector(".content")
+    contentEl.innerHTML = ""
+    menuEl.classList.add("menu")
+    menuArray.forEach(item => {
+        const menuItemEl = document.createElement("div")
+        menuItemEl.classList.add("menu-item")
+        const h2El = document.createElement("h2")
+        h2El.textContent = item.name
+
+        const ingredientsParagraphEl =  document.createElement("p")
+        ingredientsParagraphEl.classList.add("ingredients")
+        // ingredient list
+        ingredientsParagraphEl.textContent = "Ingredients"
+        const ingredientsUlEl = document.createElement("ul")
+        item.ingredients.forEach(ingredient => {
+            const ingredientLiEl = document.createElement("li")
+            ingredientLiEl.textContent = ingredient
+            ingredientsUlEl.appendChild(ingredientLiEl)
+        })
+        // the price
+        const priceParagraphEl = document.createElement("p")
+        priceParagraphEl.classList.add("price")
+        priceParagraphEl.textContent = `${item.price} TL`
+
+        // append everything to each other 
+        ingredientsParagraphEl.appendChild(ingredientsUlEl)
+        menuItemEl.appendChild(h2El)
+        menuItemEl.appendChild(ingredientsParagraphEl)
+        menuItemEl.appendChild(priceParagraphEl)
+
+        menuEl.appendChild(menuItemEl)
+
+
+    })
+
+    contentEl.appendChild(menuEl)
+
+}
+export default menuPage
